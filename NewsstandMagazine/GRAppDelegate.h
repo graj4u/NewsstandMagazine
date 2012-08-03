2b99596b0504f39c26072700202350f5bcd0bfbf
Gaurav <gauravraj@me.com>
2012-08-03 13:41:11 +0530

    Initial Commit

---
+++ NewsstandMagazine/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  NewsstandMagazine
+//
+//  Created by Gaurav Raj on 8/3/12.
+//  Copyright (c) 2012 Gaurav Raj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewsstandMagazine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NewsstandMagazine
+//
+//  Created by Gaurav Raj on 8/3/12.
+//  Copyright (c) 2012 Gaurav Raj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRAppDelegate class]));
+    }
+}
